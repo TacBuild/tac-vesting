8e82dec429b9bf6158c97387b4b8b7561056337f
Aleksei Kokinos <akkokinos@gmail.com>
2025-07-08 23:01:05 +0700

    fix test

--- scripts/config/config.ts
+++ scripts/config/config.ts
@@ -16,19 +16,19 @@ export const mainnetConfig: DeployConfig = {
 export const testnetConfig: DeployConfig = {
     crossChainLayerAddress: "0x4f3b05a601B7103CF8Fc0aBB56d042e04f222ceE",
     saFactoryAddress: "",
-    stepDuration: 60n * 1n, // 1 mins
+    stepDuration: 60n * 6n, // 6 mins
 };
 
 export const locatTestnetConfig: DeployConfig = {
     crossChainLayerAddress: "0xE65C5D7A6cb6BDF92c4B07965bCB05eB7bcC352d",
     saFactoryAddress: "0x4eAF5Dab49a7F0E0CA5A9ff61fC1A00b727D46a3",
-    stepDuration: 60n * 5n, // 5 mins
+    stepDuration: 60n * 6n, // 6 mins
 };
 
 export const localConfig: DeployConfig = {
     crossChainLayerAddress: "0xE65C5D7A6cb6BDF92c4B07965bCB05eB7bcC352d",
     saFactoryAddress: "0x4eAF5Dab49a7F0E0CA5A9ff61fC1A00b727D46a3",
-    stepDuration: 60n * 5n, // 5 mins
+    stepDuration: 60n * 6n, // 5 mins
 };
 
 export const StakingPrecompileAddress = "0x0000000000000000000000000000000000000800";
